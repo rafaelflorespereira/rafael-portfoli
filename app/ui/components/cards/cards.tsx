43c0f8f2259d26c090a65c246410b1e40f32cea7
Rafael Flores PEreira <rafaelfp84@gmail.com>
2024-07-11 10:19:06 -0300

    Update card component layout and styles

--- app/ui/components/cards/cards.tsx
+++ app/ui/components/cards/cards.tsx
@@ -24,7 +24,7 @@ export default function Card({
   };
 
   return (
-    <div className="flex p-3 odd:bg-transparent bg-slate-600 rounded-xl group relative overflow-hidden my-4">
+    <div className="flex p-3 odd:bg-transparent bg-slate-600 rounded-xl group relative overflow-hidden my-4 ">
       <div
         className="absolute w-4/5 h-full top-0 right-0 bg-gradient-to-bl from-slate-700  to-gray-800 translate-x-full group-hover:translate-x-0 invisible group-hover:visible transition-all ease-in-out rounded-r-xl 
 grid grid-rows-3 gap-2 
@@ -42,15 +42,22 @@ grid grid-rows-3 gap-2
           </div>
         ))}
       </div>
-      <Image
-        src={imageUrl}
-        alt={title}
-        width={200}
-        height={200}
-        className="rounded-full w-12 h-12 my-4 shadow-inner shadow-black"
-      />
-      <div className="w-auto p-4">
-        <p className="text-xs text-slate-400 ">{years}</p>
+      <div
+        className="rounded-full w-12 h-12 my-4 relative overflow-hidden "
+        style={{
+          boxShadow: "inset 0 0 10px rgba(0, 0, 0, 1)",
+        }}
+      >
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={200}
+          height={200}
+          className="object-cover rounded-full w-full h-full"
+        />
+      </div>
+      <div className="w-auto p-4  group-hover:[&>*]:blur-sm">
+        <p className="text-xs text-slate-400 group-hover:blur-sm">{years}</p>
         <h2 className="text-xl text-slate-200">{title}</h2>
         <p className="text-slate-300">{subtitle}</p>
         <p className="text-slate-400">{description}</p>
